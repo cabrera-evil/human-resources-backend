d0a9ba815027b5906bc440f25275a1f6c33321d1
Ricardo Herrera <00143220@uca.edu.sv>
2023-11-16 00:27:09 -0600

    Feat: user module CRUD

--- routes/users.route.js
+++ routes/users.route.js
@@ -1,9 +1,51 @@
 var express = require('express');
 var router = express.Router();
+const { check } = require("express-validator");
+const validateRequest = require("../middlewares/validateRequest.middleware");
+const { createUser, getUsers, updateUser, deleteUser } = require("../controllers/user.controller");
 
-/* GET users listing. */
-router.get('/', function(req, res, next) {
-  res.send('respond with a resource');
-});
+router.post(
+    '/',
+    [
+        // TODO: Add validation for JWT
+        check("name", "Name is required").not().isEmpty(),
+        check("email", "Email is required").not().isEmpty(),
+        check("password", "Password is required").not().isEmpty(),
+        validateRequest,
+    ],
+    createUser
+);
+
+router.get(
+    '/',
+    [
+        // TODO: Add validation for JWT
+        validateRequest,
+    ]
+    , getUsers
+);
+
+router.put(
+    '/:id',
+    [
+        // TODO: Add validation for JWT
+        check("id", "Id is required").not().isEmpty().isMongoId(),
+        check("name", "Name is required").not().isEmpty(),
+        check("email", "Email is required").not().isEmpty(),
+        check("password", "Password is required").not().isEmpty(),
+        validateRequest,
+    ],
+    updateUser
+);
+
+router.delete(
+    '/:id',
+    [
+        // TODO: Add validation for JWT
+        check("id", "Id is required").not().isEmpty().isMongoId(),
+        validateRequest,
+    ],
+    deleteUser
+);
 
 module.exports = router;
